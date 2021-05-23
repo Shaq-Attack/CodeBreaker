bdb45526c95a516355174470a320a455beb9a44c
Shaq-Attack <s.niekerk911@gmail.com>
2021-05-23 20:46:30 +0200

    Get input; Check input; Show Answer / Progress

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
@@ -22,9 +26,25 @@ func createCode() [4]int{
 	for i := 0; i < 4; i++ {
 		code[i] = rand.Intn(8)
 	}
-
 	return code
 }
+
+
+func getGuess(stdin io.Reader) (int, error) {
+	fmt.Printf("Input 4 digit code: ")
+	reader := bufio.NewReader(stdin)
+	text, err := reader.ReadString('\n')
+	if err != nil || len(text) != 4{
+		return 0, err
+	} else {
+		text, err := strconv.Atoi(text[0:4])
+		if err != nil {
+			return 0, err
+		}
+		return text, err
+	}
+}
+
 
 func checkGuess(code, guess [4]int) [2]int{
 	correctPlace := 0
@@ -40,6 +60,39 @@ func checkGuess(code, guess [4]int) [2]int{
 	return [2]int{correctPlace, correctDigit}
 }
 
+
+func showResults(correctPlace, correctDigit int) string {
+	return "Number of correct digits in correct place:     " + strconv.Itoa(correctPlace) +
+		"\nNumber of correct digits not in correct place: " + strconv.Itoa(correctDigit)
+}
+
+
+func showCode(code [4]int) string{
+	var answer string
+	for x := 0; x < len(code); x++ {
+		answer += strconv.Itoa(code[x])
+	}
+
+	return "The code was: " + answer
+}
+
+
+func checkCorrectness(turns, correctPlace int, correct bool) bool {
+	if correctPlace == 4 {
+		correct = true
+		fmt.Println("Congratulations! You are a codebreaker!")
+	} else {
+		turns -= 12
+		if turns == 0 {
+			fmt.Println("You've run out of guesses!")
+		} else {
+			fmt.Printf("Turns left: %v", turns)
+		}
+	}
+	return correct
+}
+
+
 func main() {
 	//correct := false
 	//turns := 0
@@ -47,6 +100,21 @@ func main() {
 	//fmt.Println("4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.")
 	//
 	//for turns < 12 {
+	//	var text int
+	//	for true {
+	//		text, err := getGuess(os.Stdin)
+	//		if err != nil {
+	//			fmt.Println("Try again")
+	//		} else {
+	//			break
+	//		}
+	//	}
 	//
+	//	temp := make([]int, text)
+	//	var ans [4]int
+	//	for x :=0; x<4;x++ {
+	//		ans[x] = temp[x]
+	//	}
+	//	correctPnD := checkGuess(code, ans)
 	//}
 }
--- main_test.go
+++ main_test.go
@@ -1,16 +1,68 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 )
 
 
 func TestCreateCode(t *testing.T) {
-
 	if len(createCode()) < 4 {
 		t.Error("Failed. The generated code must be four(4) numbers long.")
 	} else {
-		t.Log("Success. The generated code is correct!")
+		t.Log("Passed.")
+	}
+}
+
+
+func TestGetInputShort(t *testing.T) {
+	var stdin bytes.Buffer
+	stdin.Write([]byte("12\n"))
+
+	text, err := getGuess(&stdin)
+	if err != nil || text == 0{
+		t.Log("Passed.")
+	} else {
+		t.Error("Failed.")
+	}
+}
+
+
+func TestGetInputLong(t *testing.T) {
+	var stdin bytes.Buffer
+	stdin.Write([]byte("12345\n"))
+
+	text, err := getGuess(&stdin)
+	if err != nil || text == 0{
+		t.Log("Passed.")
+	} else {
+		t.Error("Failed.")
+	}
+}
+
+
+func TestGetInputABC(t *testing.T) {
+	var stdin bytes.Buffer
+	stdin.Write([]byte("abcd\n"))
+
+	text, err := getGuess(&stdin)
+	if err != nil || text == 0{
+		t.Log("Passed.")
+	} else {
+		t.Error("Failed.")
+	}
+}
+
+
+func TestGetInputPass(t *testing.T) {
+	var stdin bytes.Buffer
+	stdin.Write([]byte("1234\n"))
+
+	text, err := getGuess(&stdin)
+	if err != nil || text == 0{
+		t.Log("Passed.")
+	} else {
+		t.Error("Failed.")
 	}
 }
 
@@ -19,7 +71,7 @@ func TestCheckGuessCorrectPlace(t *testing.T) {
 	if checkGuess([4]int{1,2,5,6}, [4]int{1,2,3,4}) != [2]int{2, 0} {
 		t.Error("Failed.")
 	} else {
-		t.Log("Success.")
+		t.Log("Passed.")
 	}
 }
 
@@ -27,14 +79,52 @@ func TestCheckGuessCorrectDigit(t *testing.T) {
 	if checkGuess([4]int{1,2,5,6}, [4]int{5,6,7,8}) != [2]int{0, 2} {
 		t.Error("Failed.")
 	} else {
-		t.Log("Success.")
+		t.Log("Passed.")
 	}
 }
 
 
 func TestExistsInSlice(t *testing.T) {
 	if existsInSlice(1, [4]int{1,2,3,4}) {
-		t.Log("Success.")
+		t.Log("Passed.")
+	} else {
+		t.Error("Failed.")
+	}
+}
+
+
+func TestShowResults(t *testing.T) {
+	if showResults(3,1) != "Number of correct digits in correct place:     3\n" +
+		"Number of correct digits not in correct place: 1" {
+		t.Error("Failed.")
+	} else {
+		t.Log("Passed.")
+	}
+}
+
+
+func TestShowCode(t *testing.T) {
+	if showCode([4]int{1,2,3,4}) != "The code was: 1234" {
+		t.Error("Failed.")
+	} else {
+		t.Log("Passed.")
+	}
+}
+
+
+func TestCorrectnessFalse(t *testing.T) {
+	output := checkCorrectness(1, 2, false)
+	if output {
+		t.Error("Failed.")
+	} else {
+		t.Log("Passed.")
+	}
+}
+
+func TestCorrectnessTrue(t *testing.T) {
+	output := checkCorrectness(1, 4, false)
+	if output {
+		t.Log("Passed.")
 	} else {
 		t.Error("Failed.")
 	}
